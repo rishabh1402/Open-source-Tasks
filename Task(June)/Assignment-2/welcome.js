5ec1b0ff7bafeff6c4443b08e0c57e23deb40877
rishabh1402 <75234829+rishabh1402@users.noreply.github.com>
2021-09-13 17:41:36 +0530

    Update welcome.js

--- Task(June)/Assignment-2/welcome.js
+++ Task(June)/Assignment-2/welcome.js
@@ -1,24 +1,15 @@
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    
-    location.replace("index.html")
+firebase.auth().onAuthStateChanged((user) => {
+    if (user == null) {
+        location.replace("index.html")
+    } else {
+        document.getElementById("message").innerHTML = "WELCOME , " + user.email
     }
-    else{
-        document.getElementById("user").innerHTML = "Hello, "+user.email
-    }
-});
+})
 
 
-
-function logout(){
+function logout() {
     firebase.auth().signOut()
 }
-
-
 
 
 
